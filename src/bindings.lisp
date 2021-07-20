a03939cd04d07a0485bedeb9f55200b197a661c3
Lukas Herzberger <muskat.rabe+github@hotmail.com>
2021-07-21 00:52:37 +0200

    Try out windows path name

--- src/bindings.lisp
+++ src/bindings.lisp
@@ -4,7 +4,7 @@
 
 (define-foreign-library shaderc
   (:unix (:or "libshaderc_shared.so" "libshaderc_shared.so.1"))
-  (:windows "shaderc_shared.dll" :search-path "C:\VulkanSDK\*.*.*.*\Lib*\"))
+  (:windows "shaderc_shared.dll" :search-path #P"/VulkanSDK/*.*.*.*/Lib*/"))
 
 (use-foreign-library shaderc)
 
